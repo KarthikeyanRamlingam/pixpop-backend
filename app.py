17078f2c92862e3f3a34f444f016c33843f0c1bb
KarthikeyanRamlingam <150117310+KarthikeyanRamlingam@users.noreply.github.com>
2025-09-11 21:02:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-HF_API_URL = "https://huggingface.co/spaces/karthikn11/pixpop.hf.space/run/predict"
+HF_API_URL = "https://karthikn11-pixpop.hf.space/run/predict"
 
 @app.route("/")
 def home():
@@ -37,4 +37,5 @@ def generate():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))  # Railway gives $PORT
     app.run(host="0.0.0.0", port=port)
+
 
